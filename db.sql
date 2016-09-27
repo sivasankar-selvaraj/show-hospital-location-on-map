c4f45921735ca5aea6a3146e95da2966abbfa5ba
Sivasankar Selvaraj <sivasankar.selva@gmail.com>
2016-09-27 18:25:13 +0530

    Database file

---
+++ db.sql
@@ -0,0 +1,1 @@
+CREATE TABLE hospital_info(`hospital_no` INT(4) NOT NULL AUTO_INCREMENT,`hospital_name` TEXT,`category` TEXT,`hospital_address` TEXT, `pin_code` INT(7), `phone` INT(12),PRIMARY KEY (`hospital_no`)); 
